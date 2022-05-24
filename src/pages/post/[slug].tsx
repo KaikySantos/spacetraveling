63c5fecb8c1586a17ca024437179db98faefdcfa
kaikySantos <info.kaiky@gmail.com>
2022-05-24 20:58:18 -0300

    feat: page post

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,4 +1,6 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
+import { FiCalendar, FiClock, FiUser } from 'react-icons/fi';
+import Header from '../../components/Header';
 
 import { getPrismicClient } from '../../services/prismic';
 
@@ -26,9 +28,45 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post({ post }: PostProps): JSX.Element {
+  return (
+    <>
+      <Header />
+      <img src="/banner.png" alt="imagem" className={styles.banner} />
+      <main className={commonStyles.container}>
+        <div className={styles.post}>
+          <div className={styles.postTop}>
+            <h1>Algum Título</h1>
+            <ul>
+              <li>
+                <FiCalendar />
+                12 Mar 2021
+              </li>
+              <li>
+                <FiUser />
+                Kaiky Santos
+              </li>
+              <li>
+                <FiClock />5 min
+              </li>
+            </ul>
+          </div>
+
+          <article>
+            <h2>Título seção</h2>
+            <p>
+              Incididunt anim voluptate deserunt irure voluptate sit cupidatat
+              officia. Laboris enim eu Lorem do dolor laborum occaecat mollit
+              ullamco id exercitation culpa ullamco. Do aute exercitation
+              occaecat pariatur elit mollit nostrud sint ut tempor aliquip ut
+              quis occaecat.
+            </p>
+          </article>
+        </div>
+      </main>
+    </>
+  );
+}
 
 // export const getStaticPaths = async () => {
 //   const prismic = getPrismicClient({});
